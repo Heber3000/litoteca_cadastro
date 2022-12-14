91707a0fdddec0e4a6a7961733470b84a5b0e5d2
heber3000 <heberlevy@gmail.com>
2022-12-14 09:55:16 -0300

    Fazendo os rótulos(label) e saida(entry)

--- projeto.py
+++ projeto.py
@@ -23,24 +23,39 @@ janela.geometry('330x350')
 
 
 
-#Label
+#ROTULOS
+label_nome = tk.Label(janela,text='Nome da Rocha')
+label_nome.grid(row=0,column=0,padx=10,pady=10)
 
-nome_label = tk.Label(janela,text='Nome da Rochas')
-nome_label.grid(row=0,column=0,padx=10,pady=10)
+label_modaMineral = tk.Label(janela,text='Minerais(Moda)')
+label_modaMineral.grid(row=1,column=0,padx=10,pady=10)
 
-modaMineral_label = tk.Label(janela,text='Moda Mineral')
-modaMineral_label.grid(row=1,column=0,padx=10,pady=10)
+label_Local = tk.Label(janela,text='Local')
+label_Local.grid(row=2,column=0,padx=10,pady=10)
 
-local_label = tk.Label(janela,text='Local')
-local_label.grid(row=2,column=0,padx=10,pady=10)
+label_coordenadas = tk.Label(janela,text='Coordenadas')
+label_coordenadas.grid(row=3,column=0,padx=10,pady=10)
 
-coordenadas = tk.Label(janela,text='Coordenadas')
-coordenadas.grid(row=3,column=0,padx=10,pady=10)
+label_codigo = tk.Label(janela,text='Código da Amostra')
+label_codigo.grid(row=4,column=0,padx=10,pady=10)
+
 
 #ENTRADS
-
 nome_entry = tk.Entry(janela, width=35)
 nome_entry.grid(row=0,column=2,padx=10,pady=10)
 
 modaMineral_entry = tk.Entry(janela, width=35)
-nome_entry.grid(row=0,column=2,padx=10,pady=10)
+modaMineral_entry.grid(row=1,column=2,padx=10,pady=10)
+
+local_entry = tk.Entry(janela,width=35)
+local_entry.grid(row=2,column=2,padx=10,pady=10)
+
+coordenadas_entry = tk.Entry(janela,width=35)
+coordenadas_entry.grid(row=3,column=2,padx=10,pady=10)
+
+codigo_entry = tk.Entry(janela,width=35)
+codigo_entry.grid(row=4,column=2,padx=10,pady=10)
+
+
+
+janela.mainloop()
